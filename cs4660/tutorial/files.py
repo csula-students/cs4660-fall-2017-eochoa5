b63fd54b462aec5a1672cfc1f5d5399340069a0a
eochoa5 <edwinochoa57@gmail.com>
2017-09-03 18:58:16 -0700

    damn my python skills are rusty

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -4,35 +4,42 @@ class SimpleFile(object):
     """SimpleFile tests using file read api to do some simple math"""
     def __init__(self, file_path):
         self.numbers = []
-        """
-        TODO: reads the file by path and parse content into two
-        dimension array (numbers)
-        """
+
+        with open(file_path) as f:
+            content = f.read().splitlines()
+
+        self.numbers = [x.split(" ") for x in content]
+
+
 
     def get_mean(self, line_number):
         """
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
-        pass
+        x = list(map(int, self.numbers[line_number]))
+        return sum(x)/len(x)
 
     def get_max(self, line_number):
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
-        pass
+        x = list(map(int, self.numbers[line_number]))
+        return max(x)
 
     def get_min(self, line_number):
         """
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
         """
-        pass
+        x = list(map(int, self.numbers[line_number]))
+        return min(x)
 
     def get_sum(self, line_number):
         """
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
         """
-        pass
+        x = list(map(int, self.numbers[line_number]))
+        return sum(x)
---
+++ cs4660/tutorial/temp.py
@@ -0,0 +1,10 @@
+def main():
+
+    li = [1,2,3]
+
+    li2 = [li[0], li[2]]
+
+    print(li2)
+
+
+main()
